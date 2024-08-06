e91b231d2ce181d5ddf04243169b87207e7cfb58
Tomasz Łydziński <tomasz.lydzinski33@gmail.com>
2024-08-06 14:53:03 +0200

    Update 4_4

--- Zadanie_4_4.py
+++ Zadanie_4_4.py
@@ -2,56 +2,55 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 
-def calculator(s, a, b):
-    if s == "+":
-        return float(a) + float(b)
-    elif s == "-":
-        return float(a) - float(b)
-    elif s == "*":
-        return float(a) * float(b)
-    elif s == "/":
-        return float(a) / float(b)
-    
+def add(a, b):
+    logging.debug("Wybrano dodawanie")
+    logging.debug(f"Dodaję {a} i {b}")
+    return a + b
+
+def sub(a, b):
+    logging.debug("Wybrano odejmowanie")
+    logging.debug(f"Od {a} odejmuję {b}")
+    return a - b
+
+def multi(a, b):
+    logging.debug("Wybrano mnożenie")
+    logging.debug(f"Mnożę {a} i {b}")
+    return a * b
+
+def div(a, b):
+    logging.debug("Wybrano dzielenie")
+    logging.debug(f"Dzielę {a} przez {b}")
+    return a / b
+
+op_dict = {'+':add, '-':sub, '*':multi, '/':div}
+
 
 if __name__ == "__main__":
-    while True:
-        s = input("Podaj działanie, posługując się odpowiednim znakiem: Dodawanie: '+', Odejmowanie: '-', Mnożenie '*', Dzielenie '/': ")
-        if s == "+":
-            logging.debug("Wybrano dodawanie")
-            break
-        elif s == "-":
-            logging.debug("Wybrano odejmowanie")
-            break
-        elif s == "*":
-            logging.debug("Wybrano mnożenie")
-            break
-        elif s == "/":
-            logging.debug("Wybrano dzielenie")
-            break
     while True:
         a = input("Podaj składnik nr 1. Części dziesiętne oddzielaj znakiem: '.': ")
         try:
             if float(a):
                 logging.debug(f"Składnik nr 1 = {a}")
                 break
-        except:
+        except ValueError:
             continue
     while True:
         b = input("Podaj składnik nr 2. Części dziesiętne oddzielaj znakiem: '.': ")
         try:
-            if float(a):
+            if float(b):
                 logging.debug(f"Składnik nr 2 = {b}")
                 break
-        except:
+        except ValueError:
             continue
-    if s == "+":
-        logging.debug(f"Dodaję {a} i {b}")
-    elif s == "-":
-        logging.debug(f"Od {a} odejmuję {b}")
-    elif s == "*":
-        logging.debug(f"Mnożę {a} i {b}")
-    elif s == "/":
-        logging.debug(f"Dzielę {a} przez {b}")
-    logging.debug(f"Wynik wynosi = {calculator(s, a, b)}")
+
+    while True:
+        s = input("Podaj działanie, posługując się odpowiednim znakiem: Dodawanie: '+', Odejmowanie: '-', Mnożenie '*', Dzielenie '/': ")
+        if s in op_dict:
+            value = op_dict[s](float(a), float(b))
+            logging.debug(f"Wynik wynosi = {value}")
+            break
+    
+
+    
 
 
